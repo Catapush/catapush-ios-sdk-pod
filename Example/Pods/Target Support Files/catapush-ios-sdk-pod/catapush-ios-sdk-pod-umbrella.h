e9979bf8ac3425be6969a883b07f050baf58c303
Gianmaria Dal Maistro <gianmaria.dalmaistro@me.com>
2017-12-14 14:19:49 +0100

    updated library for 0.4.8

--- Example/Pods/Target Support Files/catapush-ios-sdk-pod/catapush-ios-sdk-pod-umbrella.h
+++ Example/Pods/Target Support Files/catapush-ios-sdk-pod/catapush-ios-sdk-pod-umbrella.h
@@ -1,5 +1,16 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
 
+#import "CatapushHeaders.h"
 
 FOUNDATION_EXPORT double catapush_ios_sdk_podVersionNumber;
 FOUNDATION_EXPORT const unsigned char catapush_ios_sdk_podVersionString[];
